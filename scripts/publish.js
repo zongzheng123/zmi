057ad65ccd4cb7ce941b7c29d46d615803b6ff11
宗政 <zongz@eazytec.com>
2021-02-20 11:24:21 +0800

    modify publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -5,18 +5,6 @@ const semver = require('semver')
 const semanticRelease = require('semantic-release');
 const {WritableStreamBuffer} = require('stream-buffers');
 
-function reWritePkg () {
-    const pkg = require('../package.json')
-    fs.writeFileSync(
-        path.resolve(projectPath, 'package.json'),
-        // 删除一个包之后 json会多了一些空行。sortPackage 可以删除掉并且排序
-        // prettier 会容忍一个空行
-        prettier.format(JSON.stringify(sortPackage(projectPkg)), {
-          parser: 'json',
-        }),
-      );
-}
-
 // function publish () {
 //     const gitArgs = [`publish`,]
 //     exec(
@@ -32,16 +20,16 @@ function reWritePkg () {
 //       )
 // }
 
-function publish () {
-    const stdoutBuffer = WritableStreamBuffer();
-const stderrBuffer = WritableStreamBuffer();
+async function publish () {
+    // const stdoutBuffer = WritableStreamBuffer();
+// const stderrBuffer = WritableStreamBuffer();
 
 try {
   const result = await semanticRelease({
     // Core options
     branches: [
       '+([0-9])?(.{+([0-9]),x}).x',
-      'master',
+      'main',
       'next',
       'next-major',
       {name: 'beta', prerelease: true},
@@ -49,18 +37,18 @@ try {
     ],
     repositoryUrl: 'https://github.com/zongzheng123/zmi.git',
     // Shareable config
-    extends: 'my-shareable-config',
+    // extends: 'my-shareable-config',
     // Plugin options
-    githubUrl: 'https://my-ghe.com',
-    githubApiPathPrefix: '/api-prefix'
+    // githubUrl: 'https://my-ghe.com',
+    // githubApiPathPrefix: '/api-prefix'
   }, {
     // Run semantic-release from `/path/to/git/repo/root` without having to change local process `cwd` with `process.chdir()`
-    cwd: '/path/to/git/repo/root',
+    // cwd: '/path/to/git/repo/root',
     // Pass the variable `MY_ENV_VAR` to semantic-release without having to modify the local `process.env`
     env: {...process.env, MY_ENV_VAR: 'MY_ENV_VAR_VALUE'},
     // Store stdout and stderr to use later instead of writing to `process.stdout` and `process.stderr`
-    stdout: stdoutBuffer,
-    stderr: stderrBuffer
+    // stdout: stdoutBuffer,
+    // stderr: stderrBuffer
   });
 
   if (result) {
@@ -80,13 +68,12 @@ try {
   }
 
   // Get stdout and stderr content
-  const logs = stdoutBuffer.getContentsAsString('utf8');
-  const errors = stderrBuffer.getContentsAsString('utf8');
+//   const logs = stdoutBuffer.getContentsAsString('utf8');
+//   const errors = stderrBuffer.getContentsAsString('utf8');
 } catch (err) {
   console.error('The automated release failed with %O', err)
 }
 }
 
-reWritePkg()
 publish()
 
