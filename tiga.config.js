d2cb97d51605d65158bbfce22606a3456b97607b
zongzheng <304389312@qq.com>
2021-06-04 16:20:06 +0800

    修复数据清洗

--- tiga.config.js
+++ tiga.config.js
@@ -1,6 +1,8 @@
 module.exports = {
    username: 'zongz@eazytec.com',
    password: '123456',
-   project: 'psc',
+   baseUrl: 'http://192.168.31.185:9112',
+   project: 'home',
+   model: true,
    token: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwMTAyMjQ6d2ViIiwiaWF0IjoxNjIyNTA5NDAxfQ.CqFVULj76q-ymzRP_BfqBFvodbg-u9axJYl1YlUJ_RM'
 }
